d2d1cd2ef1a53b9cd6ac75020858141c5457a4ea
jrussumbrella <jrussclay@gmail.com>
2022-05-03 08:21:30 +0800

    Implement app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from 'pages/Home';
+import JobsPage from 'pages/Jobs';
+
 function App() {
-  return <p>Hello App</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
